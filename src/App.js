fe4027a04c40fac2ef540c82b85847bc1db25719
sebuelias <sebuelias27@gmail.com>
2021-06-12 12:36:28 +0530

    updateed using immer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React, { useCallback, useRef, useState } from 'react';
-import produce from 'immer'
+import produce from 'immer';
 
 const numRows = 50;
 const numCols = 30;
 
 const cellLifeChecks = [
   [0, 1],
-  [1, 0],
+  [0, -1],
   [1, -1],
   [-1, 1],
   [1, 1],
   [-1, -1],
-  [0, -1],
+  [1, 0],
   [-1, 0],
 ];
 
@@ -20,7 +20,7 @@ const generateRandomGrid = (randomisingFactor) => {
   for (let i = 0; i < numRows; i++) {
     rows.push(
       Array.from(Array(numCols), () =>
-        Math.random() < randomisingFactor ? 1 : 0,
+        Math.random() > randomisingFactor ? 1 : 0,
       ),
     );
   }
@@ -42,45 +42,67 @@ function App() {
   });
   const [running, setRuning] = useState(false);
   // !USe this from user input
-  const [randomisingFactor, setRandomisingFactor] = useState(0.3);
+  const [randomisingFactor, setRandomisingFactor] = useState(0.8);
   const runningRef = useRef(running);
   runningRef.current = running;
 
-  const updateCell = (rowIndex, colIndex) => {
-    let newGrid = [...grid];
-    newGrid[rowIndex][colIndex] = newGrid[rowIndex][colIndex] ? 0 : 1;
-    setGrid(newGrid);
-  };
+  // const updateCell = (rowIndex, colIndex) => {
+  //   let newGrid = [...grid];
+  //   newGrid[rowIndex][colIndex] = newGrid[rowIndex][colIndex] ? 0 : 1;
+  //   setGrid(newGrid);
+  // };
 
   const simulateGameOfLife = useCallback(() => {
     if (!runningRef.current) {
       return;
     }
 
-    setGrid((grid) => {
-
-      return produce(grid, newGrid => {
-        for (let i = 0; i < numRows; i += 1) {
-          for (let j = 0; j < numCols; j += 1) {
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
             let neighbors = 0;
             cellLifeChecks.forEach(([x, y]) => {
               const newI = i + x;
-              const newJ = j + y;
-              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
-                neighbors += grid[newI][newJ];
-              }
-
-              if (neighbors < 2 || neighbors > 3) {
-                newGrid[i][j] = 0;
-              } else if (grid[i][j] === 0 && neighbors === 3) {
-                newGrid[i][j] = 1;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
               }
             });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
           }
         }
-      })
-
+      });
     });
+    // setGrid((grid) => {
+
+    //   return produce(grid, newGrid => {
+    //     for (let i = 0; i < numRows; i += 1) {
+    //       for (let j = 0; j < numCols; j += 1) {
+    //         let neighbors = 0;
+    //         cellLifeChecks.forEach(([x, y]) => {
+    //           const newI = i + x;
+    //           const newJ = j + y;
+    //           if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+    //             neighbors += grid[newI][newJ];
+    //           }
+
+    //           if (neighbors < 2 || neighbors > 3) {
+    //             newGrid[i][j] = 0;
+    //           } else if (grid[i][j] === 0 && neighbors === 3) {
+    //             newGrid[i][j] = 1;
+    //           }
+    //         });
+    //       }
+    //     }
+    //   })
+
+    // });
 
     setTimeout(simulateGameOfLife, 100);
   }, []);
@@ -123,7 +145,14 @@ function App() {
           rows.map((col, colIndex) => (
             <div
               key={`${rowIndex}-${colIndex}`}
-              onClick={() => updateCell(rowIndex, colIndex)}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[rowIndex][colIndex] = grid[rowIndex][colIndex]
+                    ? 0
+                    : 1;
+                });
+                setGrid(newGrid);
+              }}
               style={{
                 width: '20px',
                 height: '20px',
